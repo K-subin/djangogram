9ef36856e9d99646ff9b3238a8acea2fe4002bd3
kimsubin <kimsubin35@naver.com>
2020-08-02 21:57:46 +0900

    Add sign up function

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
 
 User = get_user_model()
 
@@ -28,3 +29,29 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
+
+class SignUpForm(django_forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+
+        # 레이블 이름 바꿔주기
+        labels = {
+            'email' : '이메일 주소',
+            'name' : '성명',
+            'username' : '사용자 이름',
+            'password' : '비밀번호',
+        }
+
+        # 패스워드가 보여지지 않게 하기 위해
+        widgets = {
+            'password' : django_forms.PasswordInput(),
+        }
+
+    # 유효한 패스워드 저장하기 위해 오버라이딩
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- djangogram/users/views.py
+++ djangogram/users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-
+from .forms import SignUpForm
 
 def main(request):
     # GET과 POST 분기 처리
@@ -23,4 +23,30 @@ def main(request):
         # 로그인 실패하면 메인페이지로 이동
         else:
             return render(request, 'users/main.html')
+
+# 회원가입
+def signup(request):
+    if request.method == 'GET':
+        form = SignUpForm()
+
+        return render(request, 'users/signup.html', {'form':form})
     
+    elif request.method == 'POST':
+        form = SignUpForm(request.POST) # POST와 form을 묶어줌
+
+        if form.is_valid():
+            form.save() # 저장
+
+            # 회원가입 후 자동 로그인 기능
+            # cleaned 데이터에 저장된 것을 불러온다.
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            # 로그인 성공하면 posts 페이지로 이동
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('posts:index'))
+        # 회원가입 실패하면 메인페이지로 이동            
+        return render(request, 'users/main.html')
